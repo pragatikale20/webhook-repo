606897f99cef32d54cc091cf10938c083201ec5c
pragatikale20 <pragatikale20803@gmail.com>
2025-07-01 19:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 from flask_cors import CORS
@@ -28,4 +29,5 @@ def get_events():
     return jsonify(latest_events)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
